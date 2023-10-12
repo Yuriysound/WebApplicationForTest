9811d89e3fb9f06d663b5ab144ce8cbbc59c0347
Yura songful <yura.songful@gmail.com>
2023-10-12 14:49:03 +0300

    Added Middleware

---
+++ WebApplicationForTest/Middleware/Exception/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationForTest.Middleware.Exception
+{
+    public class NotFoundException : SystemException
+    {
+        public NotFoundException(string message)
+            : base(message) { }
+    }
+}
